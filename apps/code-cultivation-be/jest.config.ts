d090674208c3260d7fe94563e257432efcfaa426
Tran Minh Phu <trminhphu79@gmail.com>
2024-12-27 01:25:39 +0700

    first commit

---
+++ apps/code-cultivation-be/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'code-cultivation-be',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/code-cultivation-be'
+};
