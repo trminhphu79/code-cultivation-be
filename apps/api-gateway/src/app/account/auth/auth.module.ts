5b3693f424928879b623dabd99008f298193bf29
michael-tran <trminhphu79@gmail.com>
2024-12-30 16:25:37 +0700

    Update throttle

--- apps/api-gateway/src/app/account/auth/auth.module.ts
+++ apps/api-gateway/src/app/account/auth/auth.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-
+import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
   controllers: [AuthController],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 100,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 500,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 10000,
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
